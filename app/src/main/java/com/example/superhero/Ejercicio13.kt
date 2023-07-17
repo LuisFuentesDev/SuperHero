bd6147142ee4b7d332900b5a2d16797e996297b9
LuisFuentesDev <lsfuentes2@gmail.com>
2023-07-17 13:42:23 -0400

    Muestra el euro

--- app/src/main/java/com/example/superhero/Ejercicio13.kt
+++ app/src/main/java/com/example/superhero/Ejercicio13.kt
@@ -1,5 +1,7 @@
 package com.example.superhero
 
+import java.text.NumberFormat
+import java.util.Currency
 import kotlin.math.ceil
 
 fun main() {
@@ -7,8 +9,19 @@ fun main() {
     println(resultado)
     var redondeo = redondeo(5.4)
     println(redondeo)
+    var muestraResultado = muestraResultado(6.7)
+    println(muestraResultado)
 }
 
 fun suma(param1: Double, param2: Float): Double = param1 + param2
 
 fun redondeo(param1: Double) = ceil(param1)
+
+fun muestraResultado(numero: Double): String {
+
+    var format: NumberFormat = NumberFormat.getCurrencyInstance()
+    format.setMaximumFractionDigits(2)
+    format.setCurrency (Currency.getInstance("EUR"))
+    println (format.format(numero))
+    return format.format(numero)
+}
